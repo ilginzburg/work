bf08ccf658fb62fb3db88a72ff5e7a8e58e7c969
ilginzburg <ilya.ginsburg@gmail.com>
2021-02-06 20:57:30 +0300

    alligned

--- 1/list/main.cpp
+++ 1/list/main.cpp
@@ -1,11 +1,7 @@
 #include <stdio.h>
 
-
-
 int main(void)
 {
-
-
    int a,b=5,c;
    float x,y=-.5,z;
     printf("a=");
@@ -24,19 +20,15 @@ int main(void)
     printf("c=%d\n",c);
     x = 5.3;
     y+=((--x-1)/x++);
-    printf("x=%f\ty=%.3f\n\n x=%.0f\ty=%.0f\n",x-1,y,x,y-1);
+    printf("x=%f\ty=%.3f\nx=%.0f\ty=%.0f\n",x-1,y,x,y-1);
     z=a/2;
-    printf(" z=%f\n",z);
+    printf("z=%f\n",z);
     z=float(a/2);
-    printf(" z=%f\n",z);
+    printf("z=%f\n",z);
     y = x/2;
-    printf(" y=%f\n",y);
+    printf("y=%f\n",y);
     y = int(x/2);
-    printf(" y=%f\n",y);
+    printf("y=%f\n",y);
     z = a%2 - (x+b)/c +(x-y)/(a-1) +1/4*a-y++ + ++b/3.;
-    printf(" a = %d b = %d c = %d x = %f y = %f z = %f\n",a,b,c,x,y,z);
-
-
-
-    return 0;
+    printf("a = %d b = %d c = %d x = %f y = %f z = %f\n",a,b,c,x,y,z);
 }
