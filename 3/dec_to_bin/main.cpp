b9c429b1a2db3080bf5389a38f68fa18cf0fcbf7
ilginzburg <ilya.ginsburg@gmail.com>
2021-02-06 22:04:57 +0300

    res var removed

--- 3/dec_to_bin/main.cpp
+++ 3/dec_to_bin/main.cpp
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
-
 // Converts number N from decimal to binary
-
 
 int main()
 {
-
     int n;
     int bin = 0;
     int k = 1;
@@ -16,11 +13,9 @@ int main()
 
     while(n!=0)
     {
-        res = n%2;
+        bin +=n%2*k;
         n /=2;
-        bin = bin+res*k;
         k *=10;
     }
-
    printf("Tne binary number is: %d\n",bin);
 }
