1db0ce2190edb7b69653f23b5f135d7555d0aa7a
ilginzburg <ilya.ginsburg@gmail.com>
2021-02-07 15:47:21 +0300

    v4

--- 2/formula/main.cpp
+++ 2/formula/main.cpp
@@ -3,9 +3,7 @@
 #define _USE_MATH_DEFINES
 
 // This program calculates expression:
-//
 // Y = ((2cos(x-pi/6) + sqrt(2)) / (1/2lnx + sin^2(x^2))) e^3x
-//
 
 int main()
 {
@@ -17,9 +15,6 @@ int main()
     scanf("%lf", &x);
     printf("\n\tX = %f\n",x);
 
-
     y = (2*cos(x-pi/6) + sqrt(2))*exp(3*x) / (1/(2*log(x)) + pow (sin( pow (x , 2)) , 2));
-
     printf("\n\tY = %f\n\n",y);
-
 }
