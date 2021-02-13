7f501bd0a7d0c9a4f71589fe9d200470e9ff0464
ilginzburg <ilya.ginsburg@gmail.com>
2021-02-13 15:31:45 +0300

    comment changed

--- 4/parentheses/main.cpp
+++ 4/parentheses/main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "parenth.h"
 
-// Check if parentheses in string are in legal order and equal
+// Check if all parentheses in string are in legal order and equal
 
 int main()
 {
