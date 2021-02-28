51fb4d01421a06001f3675194cd943cb67f66534
ilginzburg <ilya.ginsburg@gmail.com>
2021-03-01 00:23:54 +0300

    sort array removed

--- exam/task_2/childMax.cpp
+++ exam/task_2/childMax.cpp
@@ -4,24 +4,14 @@
 bool find( int* arr, int size, int child, int count )
 {
     int added = arr[child]+count;
-    insertSort(arr, size);
-    return (added >= arr[size-1]) ? true : false;
+    return (added >= findMax(arr, size)) ? true : false;
 }
 
 
-void insertSort(int* arr, int size)
+int findMax(int* arr, int size)
 {
-   int i, j;
-   int key;
-   for (i = 1; i < size; ++i)
-   {
-       key = arr[i];
-       j = i - 1;
-       while ((j >= 0) && (arr[j] > key))
-           {
-               arr[j+1] = arr[j];
-               --j;
-           }
-       arr[j+1] = key;
-   }
+   int max = 0;
+   for (int i = 0; i < size; ++i)
+       max = (arr[i] > max) ? arr[i] : max;
+   return max;
 }
--- exam/task_2/childMax.h
+++ exam/task_2/childMax.h
@@ -1,7 +1,7 @@
 #ifndef CHILDMAX_H
 #define CHILDMAX_H
 
-void insertSort(int* arr, int size);
+int findMax(int* arr, int size);
 bool find( int* arr, int size, int child, int count );
 
 #endif // CHILDMAX_H
