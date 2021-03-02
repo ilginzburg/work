d006f1472cb0277b88d5b0a0caaf15a56802177e
ilginzburg <ilya.ginsburg@gmail.com>
2021-03-02 11:08:09 +0300

    indents fixed

--- 3/dec_to_bin/calcBin.cpp
+++ 3/dec_to_bin/calcBin.cpp
@@ -4,11 +4,11 @@ int decToBin(int n)
 {
     int bin = 0;
     int k = 1;
-    while(n!=0)
+    while(n != 0)
     {
-       bin += n%2*k;
-       n /= 2;
-       k *= 10;
+        bin += n % 2 * k;
+        n /= 2;
+        k *= 10;
     }
   return bin;
 }
